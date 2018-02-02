01cd20e68b58ad69ec2abbe830b0d7de41fb2b9b
meckblvck <meckydrix@gmail.com>
2018-02-02 15:41:22 +0100

    Added the first migration

--- OrderingSystem/Startup.cs
+++ OrderingSystem/Startup.cs
@@ -27,7 +27,7 @@ namespace OrderingSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("OrderingSystemConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
