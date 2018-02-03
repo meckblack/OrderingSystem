7fee4ff6d0839a341f3804c234476edc922816b2
meckblvck <meckydrix@gmail.com>
2018-02-03 01:04:35 +0100

    Added DishesController

---
+++ OrderingSystem/Controllers/DishesController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OrderingSystem.Data;
+using OrderingSystem.Models;
+
+namespace OrderingSystem.Controllers
+{
+    public class DishesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment _environment;
+
+        #region Controller
+
+        public DishesController(ApplicationDbContext context, IHostingEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+        #endregion
+
+        #region Dish Index
+
+        // GET: Dishes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Dishes.Include(d => d.Meal);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        #endregion
+
+        #region Dish Details
+
+        // GET: Dishes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .Include(d => d.Meal)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        #endregion
+
+        #region Dish Create
+
+        // GET: Dishes/Create
+        public IActionResult Create()
+        {
+            ViewData["MealId"] = new SelectList(_context.Meals, "Id", "Image");
+            var dishes = new Dish();
+            return PartialView("Create", dishes);
+        }
+
+        // POST: Dishes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Image,Amount,MealId")] Dish dish, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                ModelState.AddModelError("null_img", "File not selected");
+            }
+            else
+            {
+                var fileinfo = new FileInfo(file.FileName);
+                var filename = DateTime.Now.ToFileTime() + fileinfo.Extension;
+                var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, filename), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    dish.Image = filename;
+                    _context.Add(dish);
+                    await _context.SaveChangesAsync();
+                    TempData["success"] = "Dish added!";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            ViewData["MealId"] = new SelectList(_context.Meals, "Id", "Image", dish.MealId);
+            return View(dish);
+        }
+
+        #endregion
+
+        #region Dish Edit
+
+        // GET: Dishes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes.SingleOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+            ViewData["MealId"] = new SelectList(_context.Meals, "Id", "Image", dish.MealId);
+            return View(dish);
+        }
+
+        // POST: Dishes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Image,Amount,MealId")] Dish dish)
+        {
+            if (id != dish.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dish);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DishExists(dish.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MealId"] = new SelectList(_context.Meals, "Id", "Image", dish.MealId);
+            return View(dish);
+        }
+
+        #endregion
+
+        #region Dish Delete
+
+        // GET: Dishes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .Include(d => d.Meal)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // POST: Dishes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dish = await _context.Dishes.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Dishes.Remove(dish);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
+        #region Dish Dispose
+
+        private bool DishExists(int id)
+        {
+            return _context.Dishes.Any(e => e.Id == id);
+        }
+
+        #endregion
+
+
+
+
+    }
+}
