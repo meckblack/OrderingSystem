24ba83cbc6e1d6781a99509477d72beb32464e4b
meckblvck <meckydrix@gmail.com>
2018-02-05 02:01:18 +0100

    Added the shop controller and related views

---
+++ OrderingSystem/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderingSystem.Data;
+
+namespace OrderingSystem.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ApplicationDbContext app;
+
+        public ShopController(ApplicationDbContext _app)
+        {
+            app = _app;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult FetchMeals()
+        {
+            var meal = app.Meals.OrderBy(s => s.Name).ToList();
+            return Json(meal);
+        }
+
+        public JsonResult FetchDishes(int id)
+        {
+            var dishes = app.Dishes.Where(s => s.MealId == id).ToList();
+            return Json(dishes);
+        }
+    }
+}
