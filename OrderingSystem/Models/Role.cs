9df88a825a6131f0e5b7e334957d6936db25ebe6
meckblvck <meckydrix@gmail.com>
2018-02-05 03:52:17 +0100

    Added Role model to the data base

---
+++ OrderingSystem/Models/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderingSystem.Models
+{
+    public class Role
+    {
+        #region Model Data
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name field is required")]
+        public string Name { get; set; }
+
+        public bool CanManageApplication { get; set; }
+
+        #endregion
+    }
+}
